ceb0aec87aead4853c82f99692d7b8c60d7234db
vicky <m.victoria_14@hotmail.com>
2020-04-05 17:02:01 +0200

    js

---
+++ JS/app.js
@@ -0,0 +1,14 @@
+//MENU MOBILE 
+
+document.querySelector('.hamburguer').addEventListener('click', getMenu)
+function getMenu(e){
+  e.preventDefault();
+  const menuToggle = document.querySelector('.main-menu').classList.toggle('menuToggleOpen') 
+}
+
+//MENU APP
+
+
+
+
+
